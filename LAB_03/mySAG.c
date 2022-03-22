d3438fea0813e1b4f1e970d83a4beb7140551a7e
André Brandão <andrembrandao@ua.pt>
2022-03-22 09:47:02 +0000

    Made min, max and average function for mySAG module

---
+++ LAB_03/mySAG.c
@@ -0,0 +1,42 @@
+#include "mySAG.h"
+#include <limits.h>
+
+int MySAGMax(int *stream, int N)
+{
+	int max = INT_MIN;
+
+	for(unsigned int i=0; i < N; i++)
+	{
+		if(stream[i] > max)
+			max = stream[i];
+	}
+
+	return max;
+}
+
+int MySAGMin(int *stream, int N)
+{
+	int min = INT_MAX;
+
+	for(unsigned int i=0; i < N; i++)
+	{
+		if(stream[i] < min)
+			min = stream[i];
+	}
+
+	return min;
+}
+
+int MySAGAvg(int *stream, int N)
+{
+
+	int sum = 0;
+
+	for(unsigned int i=0; i < N; i++)
+	{
+		sum += stream[i];
+	}
+
+	return sum / N;
+	
+}
---
+++ LAB_03/mySAG.h
@@ -0,0 +1,10 @@
+#ifndef _MYSAG_H
+#define _MYSAG_H
+
+int MySAGMax(int*, int);
+int MySAGMin(int*, int);
+
+MySAGAvg
+
+
+#endif _MYSAG_H
