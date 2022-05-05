4214ef75f248900728a09e1de21ca1043b867500
EvpSeven <evp@ua.pt>
2022-05-05 22:06:10 +0100

    Upadte Prints and Tests

---
+++ LAB_07_08/build_nrf52840dk_nrf52840/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD nrf52840dk_nrf52840 CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE /home/evp/Desktop/uni/SETR/SETR_teste/LAB_07_08/prj.conf CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE  CACHE INTERNAL "NCS child image controlled")
+set(EXTRA_KCONFIG_TARGETS menuconfig_ses CACHE INTERNAL "NCS child image controlled")
+set(EXTRA_KCONFIG_TARGET_COMMAND_FOR_menuconfig_ses /home/evp/arm_segger_embedded_studio_v568_linux_x64_nordic/html/configure_nordic_project_menuconfig.py CACHE INTERNAL "NCS child image controlled")
---
+++ LAB_07_08/build_nrf52840dk_nrf52840/zephyr/zephyr.dts.d
@@ -0,0 +1,12 @@
+empty_file.o: /home/evp/ncs/zephyr/misc/empty_file.c \
+ /home/evp/ncs/zephyr/boards/arm/nrf52840dk_nrf52840/nrf52840dk_nrf52840.dts \
+ /home/evp/ncs/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ /home/evp/ncs/zephyr/dts/common/mem.h \
+ /home/evp/ncs/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ /home/evp/ncs/zephyr/dts/arm/armv7-m.dtsi \
+ /home/evp/ncs/zephyr/dts/common/skeleton.dtsi \
+ /home/evp/ncs/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/evp/ncs/zephyr/include/dt-bindings/i2c/i2c.h \
+ /home/evp/ncs/zephyr/include/dt-bindings/gpio/gpio.h \
+ /home/evp/ncs/zephyr/include/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/evp/ncs/zephyr/dts/arm/nordic/override.dtsi
